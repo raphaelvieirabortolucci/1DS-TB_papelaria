2e6862bf188ce4b4dccf9a4944f7f04fd846b045
raphaelvieirabortolucci <raphaelvieirabortolucci@gmail.com>
2026-08-28 16:52:37 -0300

    adicao de um database e uma tabela simples

--- api/models.py
+++ api/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Categorias(models.model):
+    nome = models.CharField(max_length=100)
+    descricao = models.CharField(max_length=255)
+    ativa = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.nome
