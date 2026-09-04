0ccfda22438eb365d36b145a8b4604c8d66b5e4f
raphaelvieirabortolucci <raphaelvieirabortolucci@gmail.com>
2026-09-04 14:08:22 -0300

    adicao de uma tabelinha e link com a api e o projeto

--- api/models.py
+++ api/models.py
@@ -1,8 +1,8 @@
 from django.db import models
 
-class Categorias(models.model):
+class Categorias(models.Model):
     nome = models.CharField(max_length=100)
-    descricao = models.CharField(max_length=255)
+    descricao = models.TextField()
     ativa = models.BooleanField(default=True)
 
     def __str__(self):
